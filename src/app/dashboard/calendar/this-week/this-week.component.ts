b9702b92c811834e6b34b970719d8655423b0765
Pawel Piatek <pawelpiatek98@gmail.com>
2020-12-03 18:29:51 +0100

    added styles to this week component

--- src/app/dashboard/calendar/this-week/this-week.component.ts
+++ src/app/dashboard/calendar/this-week/this-week.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThisWeekComponent implements OnInit {
 
+  daysName = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+  days: {name: string, date: string}[] = [];
+
   constructor() { }
 
+  private initializeDaysArray(dayNumber: number) {
+
+      console.log('day number', dayNumber)
+      const today = new Date()
+      const monday =  new Date(today);
+      if(dayNumber !== 1) {
+        monday.setDate(monday.getDate() - dayNumber + 1);
+      }
+      for(let i = 0; i <7 ; i++) {
+        let currentDay = new Date(monday);
+        const nextDay = new Date(currentDay);
+        nextDay.setDate(nextDay.getDate() + i);
+        this.days.push({name: this.daysName[i], date:nextDay.toLocaleDateString()})
+        currentDay = new Date(nextDay);
+      }
+  }
+
   ngOnInit() {
+    const todayNumber = new Date().getDay();
+    this.initializeDaysArray(todayNumber);
   }
 
 }
