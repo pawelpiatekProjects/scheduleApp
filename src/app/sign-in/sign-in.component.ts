e570630655ce9be2e272f8605d5ee13f855256e0
Pawel Piatek <pawelpiatek98@gmail.com>
2020-12-20 19:33:52 +0100

    before removing interceptor

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {Router} from "@angular/router";
 
@@ -14,28 +14,76 @@ import {catchError} from "rxjs/operators";
 })
 export class SignInComponent implements OnInit {
 
-  constructor(private router: Router,  private http: HttpClient, private authService: AuthService) { }
+  constructor(private router: Router, private http: HttpClient, private authService: AuthService) {
+  }
+
+  @ViewChild('f', {static: true}) ngForm: NgForm;
+
   isLoading: boolean = false;
-  isError: boolean= false;
+  isError: boolean = false;
   errorMessage: string = null;
   inputClass: string = 'sign-in-form__item--blue';
+  emailValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+  passwordValidator = {
+    valid: true,
+    errorMessage: ''
+  }
 
   ngOnInit() {
   }
 
-  onSignIn(form: NgForm) {
-    const {value: {email, password}} = form;
-    this.isLoading = true;
-    this.authService.login(email, password).pipe(catchError(err => {
-      this.isLoading = false;
-      this.isError = true;
-      this.errorMessage = err.error.message;
-      this.inputClass = 'sign-in-form__item--red';
-      return err;
-    })).subscribe(res => {
-      this.isLoading = false;
-      this.router.navigateByUrl('/dashboard');
-    })
+  onChangeEmail() {
+    const {status, touched} = this.ngForm.form.controls.email
+    console.log(this.ngForm.form.controls.email)
+    if (status === 'INVALID' && touched === true) {
+      this.emailValidator = {
+        valid: false,
+        errorMessage: 'Email is invalid'
+      }
+    } else {
+      this.emailValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+  onChangePassword() {
+    const {status, touched} = this.ngForm.form.controls.password;
+    if (status === 'INVALID' && touched === true) {
+      this.passwordValidator = {
+        valid: false,
+        errorMessage: 'Password is invalid'
+      }
+    } else {
+      this.passwordValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+  onSignIn(f: NgForm) {
+    const {value: {email, password}, form: {controls, status}} = f;
+
+    if (status === 'VALID') {
+      console.log(status)
+      this.isLoading = true;
+      this.authService.login(email, password).pipe(catchError(err => {
+        this.isLoading = false;
+        this.isError = true;
+        this.errorMessage = err.error.message;
+        this.inputClass = 'sign-in-form__item--red';
+        return err;
+      })).subscribe(res => {
+        this.isLoading = false;
+        this.router.navigateByUrl('/dashboard');
+      })
+    }
+
 
   }
 
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthService} from "../services/auth.service";
 import {catchError} from "rxjs/operators";
@@ -12,16 +12,123 @@ import {Router} from "@angular/router";
 })
 export class SignUpComponent implements OnInit {
 
+  @ViewChild('f', {static: true}) ngForm: NgForm;
   isLoading: boolean = false;
   isError: boolean= false;
   isAccountCreated: boolean = false;
   errorMessage: string = null;
   inputClass: string = 'sign-up-form__item--blue';
 
+  nameValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+  lastNameValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+  emailValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+  phoneValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+  passwordValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+  confirmPasswordValidator = {
+    valid: true,
+    errorMessage: ''
+  }
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
+
+  onChangeName() {
+    const {status, touched} = this.ngForm.form.controls.name
+    console.log(this.ngForm.form.controls.name);
+    if (status === 'INVALID' && touched === true) {
+      this.nameValidator = {
+        valid: false,
+        errorMessage: 'Name is invalid'
+      }
+    } else {
+      this.nameValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+  onChangeLastName() {
+    const {status, touched} = this.ngForm.form.controls.lastName
+    console.log(status)
+    if (status === 'INVALID' && touched === true) {
+      this.lastNameValidator = {
+        valid: false,
+        errorMessage: 'Last name is invalid'
+      }
+    } else {
+      this.nameValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+  onChangeEmail() {
+    const {status, touched} = this.ngForm.form.controls.email
+    console.log(status)
+    if (status === 'INVALID' && touched === true) {
+      this.emailValidator = {
+        valid: false,
+        errorMessage: 'Email is invalid'
+      }
+    } else {
+      this.emailValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+  onChangePhone() {
+    const {status, touched} = this.ngForm.form.controls.phone
+    console.log(status)
+    if (status === 'INVALID' && touched === true) {
+      this.phoneValidator = {
+        valid: false,
+        errorMessage: 'Phone is invalid'
+      }
+    } else {
+      this.emailValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+  onChangePassword() {
+    const {status, touched} = this.ngForm.form.controls.password;
+    if (status === 'INVALID' && touched === true) {
+      this.passwordValidator = {
+        valid: false,
+        errorMessage: 'Password is invalid'
+      }
+    } else {
+      this.passwordValidator = {
+        valid: true,
+        errorMessage: ''
+      }
+    }
+  }
+
+
 
 
 
